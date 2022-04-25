3e39e0c2618cf0df6e25fe17be3900d32ad42efc
Luis Felipe <luis.felipe.almeida.nascimento@gmail.com>
2022-04-25 10:58:44 -0300

    episodes 1 to 15

---
+++ resources/views/posts.blade.php
@@ -0,0 +1,10 @@
+<x-layout>
+    @foreach ($posts as $post)
+        <article>
+            <h1><a href="/posts/{{$post->slug}}">{{$post->title}}</a> </h1>
+            <div>
+                {{$post->exerpt}}
+            </div>
+        </article>
+    @endforeach
+</x-layout>
