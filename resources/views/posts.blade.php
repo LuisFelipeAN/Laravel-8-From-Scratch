77956b963e8e29e52cfe00b03230ae75ab0386a9
Luis Felipe <luis.felipe.almeida.nascimento@gmail.com>
2022-04-26 15:36:46 -0300

    episode 31

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -1,4 +1,5 @@
 <x-layout>
+    <!--
     @foreach ($posts as $post)
         <article>
             <h1><a href="/posts/{{$post->id}}">{{$post->title}}</a> </h1>
@@ -8,4 +9,23 @@
             </div>
         </article>
     @endforeach
+    -->
+        @include('_posts-header')
+   
+
+        <main class="max-w-6xl mx-auto mt-6 lg:mt-20 space-y-6">
+            <x-post-featured-card/>
+
+            <div class="lg:grid lg:grid-cols-2">  
+                <x-post-card/>
+                <x-post-card/>
+            </div>
+
+            <div class="lg:grid lg:grid-cols-3">
+                <x-post-card/>
+                <x-post-card/>
+                <x-post-card/>
+            </div>
+        </main>
+
 </x-layout>
