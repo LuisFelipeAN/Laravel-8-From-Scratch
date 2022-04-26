e4e3c07fc87a3dcee93e0f1cf6c04746fc35aeb7
Luis Felipe <luis.felipe.almeida.nascimento@gmail.com>
2022-04-26 11:41:19 -0300

    episode 28

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -18,51 +18,11 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        User::truncate();
-        Category::truncate();
-        Post::truncate();
-        $user = User::factory()->create();
-
-        $personal= Category::create([
-            'name'=>'Personal',
-            'slug'=>'personal'
+        $user = User::factory()->create([
+            'name'=>'Luis Felipe'
         ]);
-
-        $familly= Category::create([
-            'name'=>'Familly',
-            'slug'=>'familly'
-        ]);
-
-        $work= Category::create([
-            'name'=>'Work',
-            'slug'=>'work'
-        ]);
-
-        Post::create([
-            'user_id'=>$user->id,
-            'category_id'=>$familly->id,
-            'title'=>'My Familly Post',
-            'slug'=>'my-familly-post',
-            'exerpt'=>'  Lorem ipsum, dolor sit',
-            'body'=>'  Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ducimus laudantium natus omnis in at inventore nostrum pariatur beatae! Maiores et dolorem nulla tenetur vel ducimus temporibus quod consequatur accusamus error.            '
-        ]);
-
-        Post::create([
-            'user_id'=>$user->id,
-            'category_id'=>$work->id,
-            'title'=>'My Work Post',
-            'slug'=>'my-work-post',
-            'exerpt'=>'  Lorem ipsum, dolor sit',
-            'body'=>'  Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ducimus laudantium natus omnis in at inventore nostrum pariatur beatae! Maiores et dolorem nulla tenetur vel ducimus temporibus quod consequatur accusamus error.            '
-        ]);
-
-        Post::create([
-            'user_id'=>$user->id,
-            'category_id'=>$personal->id,
-            'title'=>'My Personal Post',
-            'slug'=>'my-personal-post',
-            'exerpt'=>'  Lorem ipsum, dolor sit',
-            'body'=>'  Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ducimus laudantium natus omnis in at inventore nostrum pariatur beatae! Maiores et dolorem nulla tenetur vel ducimus temporibus quod consequatur accusamus error.            '
+        Post::factory(5)->create([
+            'user_id'=>$user->id
         ]);
     }
 }
